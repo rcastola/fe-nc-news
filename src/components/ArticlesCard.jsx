4013ce4b6213eaf99ecb79e40871099eb846b00b
rcastola <r.castola@gmail.com>
2023-12-12 18:08:07 +0000

    amend vote functionality to single article page following feedback

--- src/components/ArticlesCard.jsx
+++ src/components/ArticlesCard.jsx
@@ -3,34 +3,6 @@ import { patchArticle } from "../api";
 import { useState } from "react";
 
 const ArticlesCard = (article) => {
-  const [vote, setVote] = useState(null);
-  const [patchedArticle, setPatchedArticle] = useState([]);
-  const [voteError, setVoteError] = useState(false);
-
-  const upVote = (article_id) => {
-    const patchBody = { inc_votes: 1 };
-    patchArticle(article_id, patchBody)
-      .then((article) => {
-        setVote("+1");
-        setPatchedArticle(article);
-      })
-      .catch((err) => {
-        setVoteError(true);
-      });
-  };
-
-  const downVote = (article_id) => {
-    const patchBody = { inc_votes: -1 };
-    patchArticle(article_id, patchBody)
-      .then((article) => {
-        setVote("-1");
-        setPatchedArticle(article);
-      })
-      .catch((err) => {
-        setVoteError(true);
-      });
-  };
-
   return (
     <div className="article-card">
       <Link to={`/api/articles/${article.article.article_id}`}>
@@ -41,33 +13,8 @@ const ArticlesCard = (article) => {
         className="article-card-img"
       ></img>
       <p>Topic: {article.article.topic}</p>
+      <p>Votes: {article.article.votes}</p>
 
-      {vote ? (
-        <p> Votes: {patchedArticle.votes} </p>
-      ) : (
-        <p>Votes: {article.article.votes}</p>
-      )}
-      <button
-        id="upVote-button"
-        onClick={() => {
-          upVote(article.article.article_id);
-        }}
-      >
-        +1
-      </button>
-
-      <button
-        id="downVote-button"
-        onClick={() => {
-          downVote(article.article.article_id);
-        }}
-      >
-        -1
-      </button>
-
-      {vote === "+1" ? <p>Vote added.</p> : null}
-      {vote === "-1" ? <p>Vote removed.</p> : null}
-      {voteError ? <p>Unable to vote. Try again later.</p> : null}
       <p>Created at: {article.article.created_at}</p>
     </div>
   );
